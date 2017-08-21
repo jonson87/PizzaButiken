f32c03673d6d826e5331a66672b3bad24f2ab9e8
Erik Jonson <erik.jonson87@gmail.com>
2017-08-21 11:05:26 +0200

    InMemDb

---
+++ InMemDb/Data/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using InMemDb.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemDb.Data
+{
+    public static class DbInitializer
+    {
+
+        public static void Initialize(UserManager<ApplicationUser> userManager)
+        {
+            var aUser = new ApplicationUser();
+            aUser.UserName = "student@test.com";
+            aUser.Email = "student@test.com";
+            var r = userManager.CreateAsync(aUser, "Pa$$w0rd").Result;
+        }
+    }
+}
